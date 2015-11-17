9e6755532f788112ce0846b361d5304a314de160
Aleksey Usatov <usatov.aleksey@gmail.com>
2015-11-17 12:50:01 +0300

    Rewrite lexicon parser. All tests are passed.

--- test/lexicon-parser_spec.js
+++ test/lexicon-parser_spec.js
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
 import { Control } from "./../lib/symbols/control";
 import LexiconParser from '../lib/lexicon-parser';
+import { Type } from "../lib/symbols/lex_types_map";
 import fs from 'fs';
 
 describe('Lexicon parser', () => {
     describe('#makeChunk()', () => {
 
-        it(`should return instance of object with keys 'underlined', 'type', \
-            'symbol' correspoding with types bool, string and array`, () => {
+        it(`should return instance of object with keys with certain properties`, () => {
             const chunk = LexiconParser.makeChunk(true, "Some Text");
 
             expect(chunk.underlined).to.equal(true);
@@ -19,7 +19,7 @@ describe('Lexicon parser', () => {
 
     });
 
-    describe('#divideByLines', () => {
+    describe('#divideByLines()', () => {
 
         it('should divide an array of bytes by lines (by LF or CRLF)', () => {
             const buffer = fs.readFileSync(`${__dirname }/fixtures/testfile.txt`);
@@ -37,118 +37,97 @@ describe('Lexicon parser', () => {
 
     });
 
-    // describe('#divideByTrigger', () => {
-    //
-    //     it('divided lines by trigger byte', () => {
-    //
-    //     });
-    //
-    // });
-    //
-    describe('#parseString()', () => {
+    describe('#parseLine()', () => {
 
         it('should return array', () => {
-            let byteArray = new Buffer("I'm a buffer");
-            let value = parseString(byteArray);
+            const byteArray = new Buffer("I'm a buffer");
+            const value = LexiconParser.parseLine(byteArray);
 
             expect(value).to.be.an('array');
         });
 
-        it('should return array of specific objects', () => {
-            let byteArray = new Buffer("I'm a buffer");
-            let value = parseString(byteArray);
+        it(`should return array of objects with certain properties`, () => {
+            const byteArray = new Buffer("I'm a buffer");
+            const value = LexiconParser.parseLine(byteArray);
 
-            value.map((item) => {
+            value.map(item => {
                 expect(item).to.be.an('object');
                 expect(item.underlined).to.be.a('boolean');
                 expect(item.type).to.be.a('string');
                 expect(item.symbols).to.be.an('array');
             });
         });
-        
+
     });
-    //
-    // describe('#process()', () => {
-    //     it('should throw error with empty params', () => {
-    //         let value = process.bind(undefined);
-    //         expect(value).to.throw(Error);
-    //     });
-    //
-    //     it('should return array', () => {
-    //         let byteArray = new Buffer("I'm a buffer\nAnd I have some\nlines", "ascii");
-    //         let value = process(byteArray);
-    //
-    //         expect(value).to.be.an('array');
-    //     });
-    //
-    //     it('should return array with specific lenght', () => {
-    //         let byteArray = new Buffer("I'm a buffer\nAnd I have some\nlines", "ascii");
-    //         let value = process(byteArray);
-    //
-    //         expect(value.length).to.equal(3);
-    //     });
-    //
-    //     it('should return array of specific objects', () => {
-    //         let byteArray = new Buffer("I'm a buffer\nAnd I have some\nlines", "ascii");
-    //         let value = process(byteArray);
-    //
-    //         value.map((item) => {
-    //             expect(item).to.be.an('object');
-    //             expect(item.underlined).to.be.a('boolean');
-    //             expect(item.type).to.be.a('string');
-    //             expect(item.symbols).to.be.an('array');
-    //         });
-    //     });
-    //
-    //     it('should return correct array of specific objects (with CL+LF)', () => {
-    //         let byteArray = new Buffer([0x20, 0x20, Control.CL, Control.FL, 0x20, 0x20]);
-    //         let value = process(byteArray);
-    //
-    //         expect(value.length).to.equal(2);
-    //
-    //         expect(value[0].underlined).to.equal(false);
-    //         expect(value[0].type).to.equal("Normal");
-    //         expect(value[0].symbols[0]).to.equal(0x20);
-    //         expect(value[0].symbols[1]).to.equal(0x20);
-    //
-    //         expect(value[1].underlined).to.equal(false);
-    //         expect(value[1].type).to.equal("Normal");
-    //         expect(value[1].symbols[0]).to.equal(0x20);
-    //         expect(value[1].symbols[1]).to.equal(0x20);
-    //     });
-    //
-    //     it('should return correct array of specific objects (with CL)', () => {
-    //         let byteArray = new Buffer([0x20, 0x20, Control.CL, 0x20, 0x20]);
-    //         let value = process(byteArray);
-    //
-    //         expect(value.length).to.equal(2);
-    //
-    //         expect(value[0].underlined).to.equal(false);
-    //         expect(value[0].type).to.equal("Normal");
-    //         expect(value[0].symbols[0]).to.equal(0x20);
-    //         expect(value[0].symbols[1]).to.equal(0x20);
-    //
-    //         expect(value[1].underlined).to.equal(false);
-    //         expect(value[1].type).to.equal("Normal");
-    //         expect(value[1].symbols[0]).to.equal(0x20);
-    //         expect(value[1].symbols[1]).to.equal(0x20);
-    //     });
-    //
-    //     it('should return correct array of specific objects (with LF)', () => {
-    //         let byteArray = new Buffer([0x20, 0x20, Control.LF, 0x20, 0x20]);
-    //         let value = process(byteArray);
-    //
-    //         expect(value.length).to.equal(2);
-    //
-    //         expect(value[0].underlined).to.equal(false);
-    //         expect(value[0].type).to.equal("Normal");
-    //         expect(value[0].symbols[0]).to.equal(0x20);
-    //         expect(value[0].symbols[1]).to.equal(0x20);
-    //
-    //         expect(value[1].underlined).to.equal(false);
-    //         expect(value[1].type).to.equal("Normal");
-    //         expect(value[1].symbols[0]).to.equal(0x20);
-    //         expect(value[1].symbols[1]).to.equal(0x20);
-    //     });
-    // });
+
+    describe('#process()', () => {
+
+        it('should return array with length equal to number of lines in input', () => {
+            const byteArray = new Buffer("I'm a buffer\nAnd I have some\nlines", "ascii");
+            const value = LexiconParser.process(byteArray);
+
+            expect(value).to.be.an('array');
+            expect(value.length).to.equal(3);
+        });
+
+        it('should return array of specific objects', () => {
+            const byteArray = new Buffer("I'm a buffer\nAnd I have some\nlines", "ascii");
+            const value = LexiconParser.process(byteArray);
+
+            value.map(line => {
+                expect(line).to.be.an('array');
+                line.map(chunk => {
+                    expect(chunk.underlined).to.be.a('boolean');
+                    expect(chunk.type).to.be.a('string');
+                    expect(chunk.symbols).to.be.an('array');
+                })
+            });
+        });
+
+        it('should return correct array of specific objects (with CR+LF)', () => {
+            const byteArray = new Buffer([0x20, 0x20, Control.CR, Control.LF, 0x20, 0x20]);
+            const value = LexiconParser.process(byteArray);
+
+            expect(value.length).to.equal(2);
+            value.map(line => {
+                line.map(chunk => {
+                    expect(chunk.underlined).to.equal(false);
+                    expect(chunk.type).to.equal("Normal");
+                    expect(chunk.symbols[0]).to.equal(0x20);
+                    expect(chunk.symbols[1]).to.equal(0x20);
+                })
+            })
+        });
+
+        it('should return correct array of specific objects (LF)', () => {
+            const byteArray = new Buffer([0x20, 0x20, Control.LF, 0x20, 0x20]);
+            const value = LexiconParser.process(byteArray);
+
+            expect(value.length).to.equal(2);
+            value.map(line => {
+                line.map(chunk => {
+                    expect(chunk.underlined).to.equal(false);
+                    expect(chunk.type).to.equal("Normal");
+                    expect(chunk.symbols[0]).to.equal(0x20);
+                    expect(chunk.symbols[1]).to.equal(0x20);
+                })
+            })
+        });
+
+        it('should correctly parse testfile', () => {
+            const buffer = fs.readFileSync(`${__dirname }/fixtures/testfile.txt`);
+            const parsedFile = LexiconParser.process(buffer);
+
+            expect(parsedFile.length).to.equal(62);
+
+            parsedFile.map(line => {
+                expect(line).to.be.an('array');
+
+                line.map(chunk => {
+                    expect(chunk).to.be.an('object');
+                    expect(Type[chunk.type]).to.not.equal(undefined);
+                });
+            })
+        });
+    });
 });
